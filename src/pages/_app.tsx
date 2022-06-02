5b752e1a2ee5540132524bce3df98e1c4f7d0774
Edmar Alencar <edmaaralencar1@gmail.com>
2022-06-02 00:03:22 -0300

    feat: create Main file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,46 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import { useRouter } from 'next/router'
+import { NextPage } from 'next'
+import { ReactElement, ReactNode } from 'react'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
+import AuthProvider from 'context/AuthContext'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { queryClient } from 'services/queryClient'
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
+
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyles />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+
+        <AuthProvider>
+          {router.pathname.startsWith('/dashboard') ? (
+            <GlobalStyles />
+          ) : (
+            <GlobalStyles removeBg />
+          )}
+
+          <ToastContainer />
+          {getLayout(<Component {...pageProps} />)}
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
