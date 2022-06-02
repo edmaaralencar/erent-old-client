0a0e1f6b4aaae126ec24e185a169958687d224bb
Edmar Alencar <edmaaralencar1@gmail.com>
2022-06-02 00:05:50 -0300

    feat: create global styles and theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,19 +1,63 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStyleProps = {
+  removeBg?: boolean
+}
+
+const GlobalStyles = createGlobalStyle<GlobalStyleProps>`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: ${({ theme }) => theme.font.family};
   }
 
   html {
     font-size: 62.5%;
   }
 
-  body {
-    font-family: sans-serif;
+  button, a {
+    cursor: pointer;
   }
+
+  .Toastify__toast-body {
+    font-size: 1.6rem;
+  }
+
+  ${({ theme, removeBg }) => css`
+    body {
+      overflow-x: hidden;
+      color: ${theme.colors.black.main};
+
+      background-color: ${removeBg
+        ? theme.colors.white.main
+        : theme.colors.gray.lighter};
+
+      ::-webkit-scrollbar {
+        width: 5px;
+      }
+
+      ::-webkit-scrollbar-track {
+        background: #e2e2e2;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        background: #bfbfbf;
+        border-radius: 1rem;
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        background: #9e9e9e;
+      }
+
+      // -ms-overflow-style: none;
+      // scrollbar-width: none;
+
+      // &::-webkit-scrollbar {
+      //   display: none;
+      // }
+    }
+  `}
 `
 
 export default GlobalStyles
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,54 @@
-const theme = {}
+const theme = {
+  container: '130rem',
+  font: {
+    family: 'Inter, sans-serif',
+    sizes: {
+      medium: '3.6rem',
+      large: '4rem',
+      xlarge: '4.8rem',
+      huge: '7.2rem'
+    },
+    weight: {
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700
+    }
+  },
+  colors: {
+    white: {
+      main: '#FFFFFF'
+    },
+    primary: {
+      main: '#052B63',
+      dark: '#05142B',
+      darker: '#052E51'
+    },
+    gray: {
+      lighter: '#F5F8FA',
+      light: '#F4F5F7',
+      main: '#AEAEB3',
+      dark: '#7A7A80',
+      darker: '#48474A'
+    },
+    black: {
+      main: '#18191F'
+    },
+    green: {
+      main: '#03B252',
+      dark: '#027335'
+    },
+    red: {
+      main: '#DE3838',
+      dark: '#B2122C'
+    }
+  }
+}
 
 export default theme
+
+const objeto = {
+  nome: 'Maria Luísa',
+  idade: 18
+}
