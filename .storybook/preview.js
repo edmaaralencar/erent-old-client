048d0a45a508b904409449b3bf2ddfcda1ae8a11
Edmar Alencar <edmaaralencar1@gmail.com>
2022-06-02 00:00:41 -0300

    feat: basic setup with storybook, styled components and plop

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { ThemeProvider } from 'styled-components'
+
+import theme from 'styles/theme'
+import GlobalStyles from 'styles/global'
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
+}
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
