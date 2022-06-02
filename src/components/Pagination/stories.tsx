2549bba6d5b16133b95c07d77a87a7372526c84f
Edmar Alencar <edmaaralencar1@gmail.com>
2022-06-02 00:05:33 -0300

    feat: create Pagination copmonent

---
+++ src/components/Pagination/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Pagination from '.'
+
+export default {
+  title: 'Pagination',
+  component: Pagination
+} as Meta
+
+export const Default: Story = () => <Pagination />
